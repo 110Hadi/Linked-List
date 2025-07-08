0deeac1aab57e39a3e837d9621743909fd3900bd
110Hadi <hadi1raza1807@gmail.com>
2025-07-08 06:37:37 -0700

    Created The create function and a dictionary to act as a directory for all lists

--- linked-list_lib.py
+++ linked-list_lib.py
@@ -1,1 +1,25 @@
 ####Library For Linked List Functions
+
+list_directory = {
+
+
+}
+
+def create(name, length):
+    '''
+        Creates a list with the name <name> and <length> number of elements.
+         
+
+    '''
+    list_directory[name] = [[],[],length]
+
+    #### Initializing the linked list pointer array:
+
+    curr_list =  list_directory[name]
+
+    
+    for i in range(length):
+        curr_list[1].append(i + 1)
+    print(curr_list[1])
+
+create('j',2)
